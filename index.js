0cc0ae6655950315241adfaf067ba279fcadfd41
emmanuellacamp <139582917+emmanuellacamp@users.noreply.github.com>
2023-11-10 14:28:05 +0100

    add some styling and create routes for different pages

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-const featuredPost = {
-    title: "Mastering the Basics: A Beginner's Guide to Crochet",
-    content: "In this guide, we'll cover the fundamental techniques and stitches you need to know to get started on your crochet adventure..."
-  };
-  
+app.set('view engine', 'ejs');
 
-  
-app.get('/', (req, res) => {
-    res.render('home.ejs', { featuredPost });
-  });
-  
-app.get('/featuredPost', (req, res) => {
-    res.render('fullFeaturedPost', { featuredPost });
-  });
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
 })
+
+app.get("/about", (req, res)=>{
+    res.render("about")
+})
+app.get("/contact", (req, res)=>{
+    res.render("contact")
+})
+
+app.get("/compose", (req, res)=>{
+    res.render("compose")
+})
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
