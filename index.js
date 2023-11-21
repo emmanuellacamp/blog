54d3f11d921ffe1a532b98c20ea47721f1041edd
emmanuellacamp <139582917+emmanuellacamp@users.noreply.github.com>
2023-11-21 19:40:06 +0100

    redirect and render post on home page

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import lodash from "lodash";
 
 const app = express();
 const port = 3000;
+const _ = lodash();
+
+let posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,7 +16,8 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home.ejs", {posts: posts});
+
 })
 
 app.get("/about", (req, res)=>{
@@ -24,6 +29,30 @@ app.get("/contact", (req, res)=>{
 
 app.get("/compose", (req, res)=>{
     res.render("compose")
+})
+
+
+app.post("/compose", (req, res)=>{
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody,
+    }
+    posts.push(post);
+    res.redirect("/")
+})
+
+
+app.get("/posts/:postName", (req, res)=>{
+    const requestedTitle = _.lowerCase(req.params.postName);
+    posts.forEach((post) => {
+        const storedTitle = _.lowerCase(post.title);
+        if (requestedTitle === storedTitle){
+            res.render("post", {
+                title: post.title,
+                content: post.content
+            })
+        }
+    })
 })
 
 
